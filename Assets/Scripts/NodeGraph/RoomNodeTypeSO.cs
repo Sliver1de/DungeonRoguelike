9e76657d21a5809aaba790717c81556e9147a67c
Sliver1de <71007801+Sliver1de@users.noreply.github.com>
2026-05-26 22:48:29 +0800

    房间节点类型及房间节点类型列表SO

--- Assets/Scripts/NodeGraph/RoomNodeTypeSO.cs
+++ Assets/Scripts/NodeGraph/RoomNodeTypeSO.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//房间节点类型
 [CreateAssetMenu(fileName = "RoomNodeType_",menuName = "Scriptable Objects/Dungeon/Room Node Type")]
 public class RoomNodeTypeSO : ScriptableObject
 {
