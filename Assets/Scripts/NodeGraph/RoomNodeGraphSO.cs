d46999efeccfa770a73bf5c6915f2006fdfe0fa7
Sliver1de <71007801+Sliver1de@users.noreply.github.com>
2026-05-27 21:29:19 +0800

    房间节点地图编辑器

---
+++ Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//创建房间节点地图编辑器
+[CreateAssetMenu(fileName = "RoomNodeGraph",menuName = "Scriptable Objects/Dungeon/Room Node Graph")]
+public class RoomNodeGraphSO : ScriptableObject
+{
+    [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
+    [HideInInspector] public List<RoomNodeSO> roomNodeList = new List<RoomNodeSO>();
+    [HideInInspector] public Dictionary<string, RoomNodeSO> roomNodeDictionary = new Dictionary<string, RoomNodeSO>();
+
+    private void Awake()
+    {
+        LoadRoomNodeDictionary();
+    }
+
+    private void LoadRoomNodeDictionary()
+    {
+        roomNodeDictionary.Clear();
+
+        foreach (RoomNodeSO node in roomNodeList)
+        {
+            roomNodeDictionary[node.id] = node;
+        }
+    }
+
+    /// <summary>
+    /// 按房间节点类型获取房间节点
+    /// </summary>
+    /// <param name="roomNodeType"></param>
+    /// <returns></returns>
+    public RoomNodeSO GetRoomNode(RoomNodeTypeSO roomNodeType)
+    {
+        foreach (RoomNodeSO node in roomNodeList)
+        {
+            if (node.roomNodeType == roomNodeType)
+            {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// 按房间节点 ID 获取房间节点
+    /// </summary>
+    /// <param name="roomNodeID"></param>
+    /// <returns></returns>
+    public RoomNodeSO GetRoomNode(string roomNodeID)
+    {
+        if (roomNodeDictionary.TryGetValue(roomNodeID, out RoomNodeSO roomNode))
+        {
+            return roomNode;
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// 获取提供的父房间节点的子房间节点
+    /// </summary>
+    /// <param name="parentRoomNode"></param>
+    /// <returns></returns>
+    public IEnumerable<RoomNodeSO> GetChildRoomNodes(RoomNodeSO parentRoomNode)
+    {
+        foreach (string childNodeID in parentRoomNode.childRoomNodeIDList)
+        {
+            yield return GetRoomNode(childNodeID);
+        }
+    }
+
+    #region Editor Code
+#if UNITY_EDITOR
+    [HideInInspector] public RoomNodeSO roomNodeToDrawLineFrom = null;
+    [HideInInspector] public Vector2 linePosition;
+
+    //每次在编辑器中进行更改时，重新填充节点字典
+    public void OnValidate()
+    {
+        LoadRoomNodeDictionary();
+    }
+
+    public void SetNodeToDrawConnectionLineFrom(RoomNodeSO node, Vector2 position)
+    {
+        roomNodeToDrawLineFrom = node;
+        linePosition = position;
+    }
+#endif
+    #endregion
+}
