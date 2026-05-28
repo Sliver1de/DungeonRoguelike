72b2bb7cc309fb81c6672eb02ea1d140b6115d66
Sliver1de <71007801+Sliver1de@users.noreply.github.com>
2026-05-28 18:49:41 +0800

    修改RoomNodeTypeSO代码

--- Assets/Scripts/NodeGraph/RoomNodeTypeSO.cs
+++ Assets/Scripts/NodeGraph/RoomNodeTypeSO.cs
@@ -9,6 +9,7 @@ public class RoomNodeTypeSO : ScriptableObject
     public string roomNodeTypeName;
 
     #region Header
+    //仅标记应该在编辑器中可见的 RoomNodeTypes
     [Header("Only flag the RoomNodeTypes that should be visible in the editor")]
     #endregion
     public bool displayInNodeGraphEditor = true;
@@ -25,22 +26,24 @@ public class RoomNodeTypeSO : ScriptableObject
     #endregion
     public bool isCorridorEW;
     #region Header
-    [Header("One Type Should Be An Entrance")]
+    [Header("One Type Should Be An Entrance")]      //是否为入口
     #endregion
     public bool isEntrance;
     #region Header
-    [Header("One Type Should Be A Boss Room")]
+    [Header("One Type Should Be A Boss Room")]      //是否为Boss房间
     #endregion
     public bool isBossRoom;
     #region Header
-    [Header("One Type Should Be None (Unassigned)")]
+    [Header("One Type Should Be None (Unassigned)")]    //是否为空（未分配）
     #endregion
     public bool isNone;
 
     #region Validation
 #if UNITY_EDITOR
+    //OnValidate函数只在编辑器环境下触发
     private void OnValidate()
     {
+        //检测roomNodeTypeName是否被留空
         HelperUtilities.ValidateCheckEmptyString(this, nameof(roomNodeTypeName), roomNodeTypeName);
     }
 #endif
